7e5c049b4251bf3e17c16a8cb95da8979fe1e2bd
mateszucs96 <szucsmate096@gmail.com>
2024-10-14 03:30:57 +0200

    fix admin guard

--- src/app/user/guards/admin.guard.ts
+++ src/app/user/guards/admin.guard.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { UserStoreService } from '@app/user/services/user-store.service';
 
 @Injectable({
@@ -16,26 +11,11 @@ export class AdminGuard implements CanActivate {
     private router: Router
   ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    let isAdmin = false;
-    this.userStoreService.isAdmin$.subscribe(isAdminStatus => {
-      isAdmin = isAdminStatus;
-    });
-
-    if (isAdmin) {
+  canActivate(): boolean | UrlTree {
+    if (this.userStoreService.isAdmin) {
       return true;
     } else {
       return this.router.createUrlTree(['/courses']);
     }
-    // return this.userStorageService.isAdmin$.pipe(
-    //   map(isAdmin => {
-    //     console.log(isAdmin)
-    //     if (isAdmin) {
-    //       return true;
-    //     } else {
-    //       return this.router.createUrlTree(['/courses']);
-    //     }
-    //   })
-    // );
   }
 }
