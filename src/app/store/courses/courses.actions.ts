4d4058ceaee593956b4bf537ffe38bd91ff86486
mateszucs96 <szucsmate096@gmail.com>
2024-10-20 17:30:41 +0200

    add courses actions

--- src/app/store/courses/courses.actions.ts
+++ src/app/store/courses/courses.actions.ts
@@ -1,4 +1,28 @@
 import { createAction, props } from '@ngrx/store';
 import { CoursesConstants } from '@app/store/courses/courses.constants';
+import { Course } from '@shared/models/course.model';
+
+export const requestAllCourses = createAction(
+  CoursesConstants.REQUEST_ALL_COURSES
+);
+export const requestAllCoursesSuccess = createAction(
+  CoursesConstants.REQUEST_ALL_COURSES_SUCCESS,
+  props<{ courses: Course[] }>()
+);
+export const REQUEST_ALL_COURSES_FAIL = createAction(
+  CoursesConstants.REQUEST_ALL_COURSES_FAIL
+);
+
+export const requestSingleCourse = createAction(
+  CoursesConstants.REQUEST_SINGLE_COURSE,
+  props<{ courseId: string | null }>()
+);
+export const requestSingleCourseSuccess = createAction(
+  CoursesConstants.REQUEST_SINGLE_COURSE_SUCCESS,
+  props<{ course: Course }>()
+);
+export const requestSingleCourseFail = createAction(
+  CoursesConstants.REQUEST_SINGLE_COURSE_FAIL
+);
 
 // Add your code here
