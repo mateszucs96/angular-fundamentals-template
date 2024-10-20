7fe33ea360e1aca031b023aef79f5af56e681fe5
mateszucs96 <szucsmate096@gmail.com>
2024-10-20 18:00:34 +0200

    add all course actions

--- src/app/store/courses/courses.actions.ts
+++ src/app/store/courses/courses.actions.ts
@@ -3,26 +3,79 @@ import { CoursesConstants } from '@app/store/courses/courses.constants';
 import { Course } from '@shared/models/course.model';
 
 export const requestAllCourses = createAction(
-  CoursesConstants.REQUEST_ALL_COURSES
+  CoursesConstants.REQUEST_ALL_COURSES,
+  props<{ courses: Course[] }>()
 );
 export const requestAllCoursesSuccess = createAction(
   CoursesConstants.REQUEST_ALL_COURSES_SUCCESS,
   props<{ courses: Course[] }>()
 );
-export const REQUEST_ALL_COURSES_FAIL = createAction(
-  CoursesConstants.REQUEST_ALL_COURSES_FAIL
+export const requestAllCoursesFail = createAction(
+  CoursesConstants.REQUEST_ALL_COURSES_FAIL,
+  props<{ error: string }>()
 );
 
 export const requestSingleCourse = createAction(
   CoursesConstants.REQUEST_SINGLE_COURSE,
-  props<{ courseId: string | null }>()
+  props<{ courseId: string }>()
 );
 export const requestSingleCourseSuccess = createAction(
   CoursesConstants.REQUEST_SINGLE_COURSE_SUCCESS,
   props<{ course: Course }>()
 );
 export const requestSingleCourseFail = createAction(
-  CoursesConstants.REQUEST_SINGLE_COURSE_FAIL
+  CoursesConstants.REQUEST_SINGLE_COURSE_FAIL,
+  props<{ error: string }>()
 );
 
-// Add your code here
+export const requestFilteredCourses = createAction(
+  CoursesConstants.REQUEST_FILTERED_COURSES,
+  props<{ title: string }>()
+);
+export const requestFilteredCoursesSuccess = createAction(
+  CoursesConstants.REQUEST_FILTERED_COURSES_SUCCESS,
+  props<{ filteredCourses: Course[] }>()
+);
+export const requestFilteredCoursesFail = createAction(
+  CoursesConstants.REQUEST_FILTERED_COURSES_FAIL,
+  props<{ error: string }>()
+);
+
+export const requestDeleteCourse = createAction(
+  CoursesConstants.REQUEST_DELETE_COURSE,
+  props<{ id: string }>()
+);
+export const requestDeleteCourseSuccess = createAction(
+  CoursesConstants.REQUEST_DELETE_COURSE_SUCCESS,
+  props<{ id: string }>()
+);
+export const requestDeleteCourseFail = createAction(
+  CoursesConstants.REQUEST_DELETE_COURSE_FAIL,
+  props<{ error: string }>()
+);
+
+export const requestCreateCourse = createAction(
+  CoursesConstants.REQUEST_CREATE_COURSE,
+  props<{ course: Course }>()
+);
+export const requestCreateCourseSuccess = createAction(
+  CoursesConstants.REQUEST_CREATE_COURSE_SUCCESS,
+  props<{ course: Course }>()
+);
+export const requestCreateCourseFail = createAction(
+  CoursesConstants.REQUEST_CREATE_COURSE_FAIL,
+  props<{ error: string }>()
+);
+
+export const requestEditCourse = createAction(
+  CoursesConstants.REQUEST_EDIT_COURSE,
+  props<{ course: Course }>()
+);
+export const requestEditCourseSuccess = createAction(
+  CoursesConstants.REQUEST_EDIT_COURSE_SUCCESS,
+  props<{ course: Course }>()
+);
+export const requestEditCourseFail = createAction(
+  CoursesConstants.REQUEST_EDIT_COURSE_FAIL,
+  props<{ error: string }>()
+);
--- src/app/store/courses/courses.reducer.ts
+++ src/app/store/courses/courses.reducer.ts
@@ -1,19 +1,38 @@
-import { Action } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
+import { Course } from '@shared/models/course.model';
+import * as CourseActions from './courses.actions';
+import { requestAllCourses } from '@app/store/courses/courses.actions';
 
 // Add your code here
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface CoursesState {
-  // Add your code here
+  allCourses: Course[];
+  course: Course | null;
+  isAllCoursesLoading: boolean;
+  isSingleCourseLoading: boolean;
+  isSearchState: boolean;
+  errorMessage: string | null;
 }
 
 export const initialState: CoursesState = {
-  // Add your code here
+  allCourses: [],
+  course: null,
+  isAllCoursesLoading: false,
+  isSingleCourseLoading: false,
+  isSearchState: false,
+  errorMessage: null,
 };
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export const coursesReducer; // Add your code here
+export const coursesReducer = createReducer(
+  initialState,
+  on(CourseActions.requestAllCourses, state => ({
+    ...state,
+    isAllCoursesLoading: true,
+  }))
+); // Add your code here
 
 export const reducer = (state: CoursesState, action: Action): CoursesState =>
   coursesReducer(state, action);
