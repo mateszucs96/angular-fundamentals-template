9333ffdc72074fefac5fb60c778bf405f54c9bba
mateszucs96 <szucsmate096@gmail.com>
2024-10-20 18:17:36 +0200

    add inital state, and implement reducer functionality

--- src/app/store/courses/courses.reducer.ts
+++ src/app/store/courses/courses.reducer.ts
@@ -1,7 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { Course } from '@shared/models/course.model';
 import * as CourseActions from './courses.actions';
-import { requestAllCourses } from '@app/store/courses/courses.actions';
 
 // Add your code here
 
@@ -28,9 +27,112 @@ export const initialState: CoursesState = {
 // @ts-ignore
 export const coursesReducer = createReducer(
   initialState,
+
+  //All
   on(CourseActions.requestAllCourses, state => ({
     ...state,
     isAllCoursesLoading: true,
+  })),
+  on(CourseActions.requestAllCoursesSuccess, (state, { courses }) => ({
+    ...state,
+    allCourses: courses,
+    isAllCoursesLoading: false,
+  })),
+  on(CourseActions.requestAllCoursesFail, (state, { error }) => ({
+    ...state,
+    isAllCoursesLoading: false,
+    errorMessage: error,
+  })),
+
+  // single
+
+  on(CourseActions.requestSingleCourse, state => ({
+    ...state,
+    isSingleCourseLoading: true,
+  })),
+  on(CourseActions.requestSingleCourseSuccess, (state, { course }) => ({
+    ...state,
+    course: course,
+    isSingleCourseLoading: false,
+  })),
+  on(CourseActions.requestSingleCourseFail, (state, { error }) => ({
+    ...state,
+    isSingleCourseLoading: false,
+    errorMessage: error,
+  })),
+
+  // filter
+
+  on(CourseActions.requestFilteredCourses, state => ({
+    ...state,
+    isAllCoursesLoading: true,
+    isSearchState: true,
+  })),
+  on(
+    CourseActions.requestFilteredCoursesSuccess,
+    (state, { filteredCourses }) => ({
+      ...state,
+      allCourses: filteredCourses,
+      isAllCoursesLoading: false,
+      isSearchState: false,
+    })
+  ),
+  on(CourseActions.requestFilteredCoursesFail, (state, { error }) => ({
+    ...state,
+    isAllCoursesLoading: false,
+    isSearchState: false,
+    errorMessage: error,
+  })),
+
+  // create
+
+  on(CourseActions.requestCreateCourse, state => ({
+    ...state,
+    isAllCoursesLoading: true,
+  })),
+  on(CourseActions.requestCreateCourseSuccess, (state, { course }) => ({
+    ...state,
+    course: course,
+    isAllCoursesLoading: false,
+  })),
+  on(CourseActions.requestCreateCourseFail, (state, { error }) => ({
+    ...state,
+    isAllCoursesLoading: false,
+    errorMessage: error,
+  })),
+
+  // edit
+
+  on(CourseActions.requestEditCourse, state => ({
+    ...state,
+    isAllCoursesLoading: true,
+  })),
+  on(CourseActions.requestEditCourseSuccess, (state, { course }) => ({
+    ...state,
+    course: course,
+    isAllCoursesLoading: false,
+  })),
+  on(CourseActions.requestEditCourseFail, (state, { error }) => ({
+    ...state,
+    isAllCoursesLoading: false,
+    errorMessage: error,
+  })),
+
+  // delete
+
+  on(CourseActions.requestDeleteCourse, state => ({
+    ...state,
+    isAllCoursesLoading: true,
+  })),
+  on(CourseActions.requestDeleteCourseSuccess, (state, { id }) => ({
+    ...state,
+    allCourses: state.allCourses.filter(courses => courses.id !== id),
+    isAllCoursesLoading: false,
+  })),
+  on(CourseActions.requestDeleteCourseFail, (state, { error }) => ({
+    ...state,
+    isAllCoursesLoading: false,
+    errorMessage: error,
   }))
 ); // Add your code here
 
